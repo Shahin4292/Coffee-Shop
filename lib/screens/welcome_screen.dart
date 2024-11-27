e4c8cf4531b105c5c008ff2a18a5b220c56f48f5
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-27 19:08:11 +0600

    add elevated button

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,25 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        // padding: const EdgeInsets.only(top: 100, bottom: 40),
+    return Material(
+      child: Container(
+        padding: const EdgeInsets.only(top: 100, bottom: 40),
         decoration: const BoxDecoration(
             color: Colors.black,
             image: DecorationImage(
                 image: AssetImage('assets/images/bg.png'),
                 fit: BoxFit.cover,
                 opacity: 0.6)),
-        child: const Column(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
               "Coffee Shop",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: GoogleFonts.pacifico(
+                fontSize: 50,
+                color: Colors.white,
+              ),
             ),
+            Column(
+              children: [
+                Text(
+                  "Feeling Low? Take a Cup of Coffee",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 1,
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                ),
+                const SizedBox(
+                  height: 80,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      backgroundColor: Colors.green,
+                      fixedSize: const Size(200, 50),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Get Start",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1),
+                    ))
+              ],
+            )
           ],
         ),
       ),
