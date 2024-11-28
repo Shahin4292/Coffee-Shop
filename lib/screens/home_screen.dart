8571cfe90c9c2a00249c37d8d03a4dc0e2e23d76
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-28 08:45:37 +0600

    search input field done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,6 +27,39 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          children: [
+            const Text(
+              "It's a Great Day for Coffee",
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 30,
+                  color: Colors.white),
+            ),
+            Container(
+              width: MediaQuery.sizeOf(context).width,
+              margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color.fromARGB(255, 50, 54, 56),
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Find your Coffee",
+                  hintStyle: TextStyle(
+                    color: Colors.white.withOpacity(0.5),
+                  ),
+                  prefixIcon: Icon(Icons.search,
+                      size: 25, color: Colors.white.withOpacity(0.5)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
