1614292fc547a53daa93e534dd1a6f5f06080cea
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-28 13:04:09 +0600

    done ui

---
+++ lib/screens/single_items_screen.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SingleItemsScreen extends StatelessWidget {
+  String img;
+
+  SingleItemsScreen(this.img, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30, bottom: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white.withOpacity(0.5),
+                      )),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Center(
+                  child: Image.asset(
+                    "assets/images/$img.png",
+                    width: MediaQuery.sizeOf(context).width / 1.2,
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25, right: 40),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "BEST COFFEE",
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.4),
+                            letterSpacing: 3),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        img,
+                        style: const TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                            letterSpacing: 1),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        width: MediaQuery.sizeOf(context).width,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(
+                                  color: Colors.white.withOpacity(0.2),
+                                ),
+                              ),
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                        CupertinoIcons.minus,
+                                        size: 18,
+                                        color: Colors.white,
+                                      )),
+                                  const SizedBox(
+                                    width: 15,
+                                  ),
+                                  TextButton(
+                                      onPressed: () {},
+                                      child: const Text(
+                                        "2",
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.white,
+                                        ),
+                                      )),
+                                  const SizedBox(
+                                    width: 15,
+                                  ),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                        CupertinoIcons.minus,
+                                        size: 18,
+                                        color: Colors.white,
+                                      )),
+                                ],
+                              ),
+                            ),
+                            const Text(
+                              "\$30.20",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Coffee is a major source of antioxidants in the diet. It has many health benefit",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white.withOpacity(0.4),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Row(
+                        children: [
+                          Text(
+                            "Volume",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "60 ml",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 60,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.sizeOf(context).width,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 20, horizontal: 50),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(18),
+                                  color: const Color.fromARGB(255, 50, 54, 56)),
+                              child: const Text(
+                                "Add To Card",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  letterSpacing: 1,
+                                ),
+                              ),
+                            ),
+                            Container(
+                                padding: EdgeInsets.all(18),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(18),
+                                    color:
+                                        const Color(0xFFE57734)),
+                                child: IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.favorite_outline,
+                                    color: Colors.white,
+                                  ),
+                                ))
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
