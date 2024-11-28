3afb73f5a5d0c0047a1ca86f48271515c22c53e9
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-28 10:44:23 +0600

    items widgets almost done

---
+++ lib/widgets/items_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ItemsWidget extends StatelessWidget {
+  ItemsWidget({super.key});
+
+  List img = ["Latte", "Espresso", "Cold Coffee", "Black Coffee"];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+        physics: const NeverScrollableScrollPhysics(),
+        crossAxisCount: 2,
+        shrinkWrap: true,
+        childAspectRatio: (150 / 195),
+        children: [
+          for (int i = 0; i < img.length; i++)
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 13),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xFF212325),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.4),
+                      spreadRadius: 1,
+                      blurRadius: 8,
+                    )
+                  ]),
+            )
+        ]);
+  }
+}
