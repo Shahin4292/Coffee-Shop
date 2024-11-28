163077dc64940c2b624963115c854d36504df1ad
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-28 09:23:37 +0600

    tapBar add done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 0);
+    _tabController.addListener(_handleTabSelection);
+    super.initState();
+  }
+
+  _handleTabSelection() {
+    if (_tabController.indexIsChanging) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,38 +54,74 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        child: Column(
-          children: [
-            const Text(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Text(
               "It's a Great Day for Coffee",
               style: TextStyle(
                   fontWeight: FontWeight.w500,
                   fontSize: 30,
                   color: Colors.white),
             ),
-            Container(
-              width: MediaQuery.sizeOf(context).width,
-              margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: const Color.fromARGB(255, 50, 54, 56),
-              ),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Find your Coffee",
-                  hintStyle: TextStyle(
-                    color: Colors.white.withOpacity(0.5),
-                  ),
-                  prefixIcon: Icon(Icons.search,
-                      size: 25, color: Colors.white.withOpacity(0.5)),
+          ),
+          Container(
+            width: MediaQuery.sizeOf(context).width,
+            margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: const Color.fromARGB(255, 50, 54, 56),
+            ),
+            child: TextFormField(
+              style:  TextStyle(
+                  color: Colors.white.withOpacity(0.5),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Find your Coffee",
+                hintStyle: TextStyle(
+                  color: Colors.white.withOpacity(0.5),
                 ),
+                prefixIcon: Icon(Icons.search,
+                    size: 25, color: Colors.white.withOpacity(0.5)),
               ),
             ),
-          ],
-        ),
+          ),
+          TabBar(
+              controller: _tabController,
+              labelColor: const Color(0xFFE57734),
+              unselectedLabelColor: Colors.white.withOpacity(0.5),
+              isScrollable: true,
+              indicator: const UnderlineTabIndicator(
+                  borderSide: BorderSide(
+                width: 3,
+                color: Color(0xFFE57734),
+              )),
+              dividerColor: Colors.transparent,
+              labelStyle:
+                  const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              labelPadding: const EdgeInsets.symmetric(horizontal: 20),
+              tabs: const [
+                Tab(
+                  text: "Hot Coffee",
+                ),
+                Tab(
+                  text: "Cold Coffee",
+                ),
+                Tab(
+                  text: "Cappuccino",
+                ),
+                Tab(
+                  text: "Americano",
+                ),
+              ]),
+          const SizedBox(
+            height: 10,
+          ),
+        ],
       ),
     );
   }
